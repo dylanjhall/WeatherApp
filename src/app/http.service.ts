ab704e999662b8deb490813fec18dea9909835f2
dylanjhall <dylanjhall@gmail.com>
2021-10-15 22:43:16 -0400

    Refactored location of response interfaces

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { WeatherInfo } from './display-forecast/display-forecast.component';
+import { WeatherInfo } from './response-interfaces/response-interfaces';
+
 
 @Injectable({
   providedIn: 'root'
