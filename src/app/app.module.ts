25216f662ecdbd5ee6aa352753a93d2826591725
John Stamatelos <john@tangentsolutions.co.za>
2022-02-05 14:09:55 +0200

    feat(app): remove bootstrap and ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigComponent } from './config/config.component';
 import { DisplayCurrentWeatherComponent } from './display-current-weather/display-current-weather.component';
-import { NgbModule, NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 import { CitySearchComponent } from './city-search/city-search.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CitySearchModule } from './city-search/city-search.module';
 
 
 @NgModule({
@@ -17,14 +17,13 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     AppComponent,
     ConfigComponent,
     DisplayCurrentWeatherComponent,
-    CitySearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    CitySearchModule
 
   ],
   providers: [HttpClientModule],
--- src/app/city-search/city-search.module.ts
+++ src/app/city-search/city-search.module.ts
@@ -1,17 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
 import { CitySearchComponent } from './city-search.component';
 
-
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbModule, CommonModule, NgModule],
+  imports: [FormsModule, CommonModule],
   declarations: [CitySearchComponent],
   exports: [CitySearchComponent],
-  bootstrap: [CitySearchComponent]
 })
 export class CitySearchModule {}
