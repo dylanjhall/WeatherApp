69459df3ba1ed169e8bc7d1891159d0dd1e17510
John Stamatelos <john@tangentsolutions.co.za>
2022-02-11 22:54:57 +0200

    feat(app): add FlexLayoutModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitySearchModule } from './city-search/city-search.module';
 import { ConfigComponent } from './config/config.component';
 import { DisplayCurrentWeatherComponent } from './display-current-weather/display-current-weather.component';
-import { CitySearchComponent } from './city-search/city-search.component';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { CitySearchModule } from './city-search/city-search.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [AppComponent, ConfigComponent, DisplayCurrentWeatherComponent],
   imports: [
@@ -24,7 +22,7 @@ import { MatIconModule } from '@angular/material/icon';
     CitySearchModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule,
+    FlexLayoutModule,
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent],
